5cba839ab3a9139ab50c02adfaa748f8d6c477c9
RyinckevichDaniil <abdul.alahakbar@gmail.com>
2020-07-01 21:30:01 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
+let height = +prompt('Введите ваше число:');
+
 function xmasTree(pyramidHeight, symbol) {
 
-    var height = Number(prompt('Введите ваше число:'));
-
-    var xmas = '';
-    for (let i = 0; i <= height; i++) {
+    let xmas = '';
+    for (let i = 1; i <= height; i++) {
       let row = '';
-      for (j = 0; j <= i; j++) {
+      for (j = 1; j <= i; j++) {
         row = row += '*';
+        // let t = '';
+        // while (t = j) {
+        //   row = row += ' ';
+        //   t++
+        // }
       }
       xmas = xmas += row + '\n';
     }
     
-    alert(xmas);
     console.log(xmas);
-
+    alert(xmas);
 }
 
 // Check your code
